8e92e720301cd8f2bbb5058f9d5cca2f02571a3d
Thiago Alves dos Santos <tasantos@mppe.mp.br>
2024-10-19 21:11:34 -0300

    refactoring onehotencoding

---
+++ bitnet/utils.py
@@ -0,0 +1,38 @@
+
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Source https://github.com/mrdbourke/pytorch-deep-learning/blob/main/helper_functions.py
+# Com modificações p/ esperar y sempre em One Hot Encoding
+def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
+    """Plots decision boundaries of model predicting on X in comparison to y.
+
+    Source - https://madewithml.com/courses/foundations/neural-networks/ (with modifications)
+    """
+    # Put everything to CPU (works better with NumPy + Matplotlib)
+    model.to("cpu")
+    X, y = X.to("cpu"), y.to("cpu")
+
+    # Setup prediction boundaries and grid
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+
+    # Make features
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+
+    # Make predictions
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X_to_pred_on)
+
+    # Modificação p/ sempre tratar y_pred como vector
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)  # mutli-class
+
+    # Reshape preds and plot
+    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
